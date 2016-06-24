c8796cdb6fa4936e615a5cb94fa2eb02668b5346
Thendup Tsering <thendup@thinkdataworks.com>
2016-06-24 01:30:19 -0400

    Install bower bootstrap component and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Client.create([
+#   {
+#     name: 'Toyota Inc.',
+#     address: '78 Dune Drive',
+#     entity_type: 'Business',
+#     email: 'info@toyota.com',
+#     phone_no: '226 233 9999',
+#     fax_no: '226 322 9998',
+#     gender: '',
+#     career: 'N/A',
+#     birthday: '1980-06-20',
+#     entry_point: 'N/A',
+#     entry_date: '2016-06-23',
+#     size: 1500,
+#     contact_name: 'Tom Bailey',
+#     industry: 'Auto Manufacturer',
+#   },
+#   {
+#       name: 'Thendup Tsering',
+#       address: '260 Regina Street',
+#       entity_type: 'Consumer',
+#       email: 'thendup11@gmail.com',
+#       phone_no: '519 580 6151',
+#       fax_no: 'N/A',
+#       gender: 'Male',
+#       career: 'Developer',
+#       birthday: '1980-06-20',
+#       entry_point: 'Social Media',
+#       entry_date: '2016-06-23',
+#       size: 1,
+#       contact_name: 'N/A',
+#       industry: 'N/A',
+#     }
+# ])
